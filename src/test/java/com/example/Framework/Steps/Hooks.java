6b87a7094e1a9414b0b9d6395ccb640bbd375959
nsathya <snmoorthy108@gmail.com>
2026-08-16 23:16:32 -0500

    Fix Hooks to run Chrome headless in CI, remove problematic maximize()

--- src/test/java/com/example/Framework/Steps/Hooks.java
+++ src/test/java/com/example/Framework/Steps/Hooks.java
@@ -6,6 +6,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     private final TestContext context;
@@ -16,12 +17,17 @@ public class Hooks {
 
     @Before
     public void cucumber_setUp() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
         this.context.driver = driver;
         driver.get(ConfigReader.getProperty("baseUrl"));
         driver.manage().deleteAllCookies();
         driver.navigate().refresh();
-        driver.manage().window().maximize();
         Thread.sleep(2000);
 
     }
